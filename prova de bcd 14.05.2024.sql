2deeb72c0ba14afc492e52016450f7462e632700
miriamfernandes22c <165180767+miriamfernandes22c@users.noreply.github.com>
2024-05-14 18:16:26 -0300

    Add files via upload

---
+++ prova de bcd 14.05.2024.sql
@@ -0,0 +1,18 @@
+
+use avaliacao_22c;
+CREATE TABLE livros;
+id INT AUTO_INCREMENT PRIMARY KEY,
+titulo VARCHAR (255),
+auto VARCHAR (255),
+ano publi.INT,
+DELETE ano e idiomas,
+LIMIT (5),
+WHERE (600),
+editora INT VARCHAR (255),
+qntd_pgs INT,
+idioma VARCHAR (255)
+categoria GROUP BY,
+paginas WHERE (600),
+genero, autor, categoria, ano, idioma, lançamento ORDER BY,
+SELECT datas entre 1970 e 1990 e terminados em "S"
+INSERT INTO ("título, autor, ano_publicação, disponibilidade, categoria, editora, quantidade")
